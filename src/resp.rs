367eaf374689317bcfeba0648d844434d6d8a351
Bobby R. Ward <bobbyrward@gmail.com>
2020-11-19 19:38:07 -0600

    Experimenting with different parser approaches

--- src/resp.rs
+++ src/resp.rs
@@ -2,6 +2,275 @@ use bytes::{Buf, BytesMut};
 
 use crate::error::EngineError;
 
+/*
+struct State_WaitingForType {}
+struct State_ReadInteger { buffer: Vec<u8> }
+struct State_ReadSimpleString { buffer: Vec<u8> }
+struct State_ReadErrorValue { buffer: Vec<u8> }
+struct State_ReadBulkStringSize { buffer: Vec<u8> }
+struct State_ReadBulkStringData { length: usize, buffer: Vec<u8> }
+struct State_ReadArraySize { buffer: Vec<u8> }
+struct State_ReadArrayPart { length: usize, index: usize }
+
+struct StateBasedParser<State> {
+    state: State,
+    buffer: Vec<u8>,
+}
+
+impl<State> StateBasedParser<State> {
+}
+
+impl StateBasedParser<WaitingForType> {
+    fn process(&mut self, source: &mut BytesMut) -> Result<Option<RespValue>, EngineError> {
+
+    }
+}
+*/
+
+#[derive(Debug, Clone)]
+enum ParserState {
+    WaitingForType,
+    ReadInteger,
+    ReadSimpleString,
+    ReadErrorValue,
+    ReadBulkStringSize,
+    ReadBulkStringData {
+        length: usize,
+    },
+    ReadArraySize,
+    ReadArrayPart {
+        length: usize,
+        index: usize,
+        values: Vec<RespValue>,
+        subparser: Box<Parser>,
+    },
+    ParseComplete {
+        value: RespValue,
+    },
+}
+
+impl ParserState {
+    fn into_value(self) -> RespValue {
+        match self {
+            Self::ParseComplete { value } => value,
+            _ => panic!("State is not ParseComplete"),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Parser {
+    state: ParserState,
+    buffer: Vec<u8>,
+}
+
+impl Parser {
+    fn new() -> Self {
+        Self {
+            state: ParserState::WaitingForType,
+            buffer: Vec::with_capacity(1024),
+        }
+    }
+
+    fn read_to_crlf(&mut self, source: &mut BytesMut) -> Option<&[u8]> {
+        if source.len() < 2 {
+            self.buffer.extend_from_slice(&source.split()[..]);
+        } else {
+            let newline_offset = source
+                .iter()
+                .zip(source.iter().skip(1))
+                .position(|(cr, lf)| *cr == b'\r' && *lf == b'\n');
+
+            if let Some(offset) = newline_offset {
+                self.buffer.extend_from_slice(&source.split_to(offset)[..]);
+                source.advance(2);
+
+                return Some(&self.buffer[..]);
+            } else {
+                self.buffer.extend_from_slice(&source.split()[..]);
+            }
+        }
+
+        None
+    }
+
+    fn process(&mut self, source: &mut BytesMut) -> Result<Option<RespValue>, EngineError> {
+        match self.state {
+            ParserState::WaitingForType => {
+                if !source.is_empty() {
+                    self.buffer.clear();
+
+                    self.state = match source.get_u8() {
+                        b':' => ParserState::ReadInteger,
+                        b'+' => ParserState::ReadSimpleString,
+                        b'-' => ParserState::ReadErrorValue,
+                        b'$' => ParserState::ReadBulkStringSize,
+                        b'*' => ParserState::ReadArraySize,
+                        x => {
+                            return Err(EngineError::unknown(&format!(
+                                "unknown type: {} {:?}",
+                                x,
+                                std::char::from_u32(x.into())
+                            )))
+                        }
+                    };
+
+                    return self.process(source);
+                }
+            }
+            ParserState::ReadSimpleString => {
+                if let Some(value) = self.read_to_crlf(source) {
+                    self.state = ParserState::ParseComplete {
+                        value: RespValue::SimpleString(utf8(value)?),
+                    };
+                    return self.process(source);
+                }
+            }
+            ParserState::ReadErrorValue => {
+                if let Some(value) = self.read_to_crlf(source) {
+                    self.state = ParserState::ParseComplete {
+                        value: RespValue::Error(utf8(value)?),
+                    };
+                    return self.process(source);
+                }
+            }
+            ParserState::ReadInteger => {
+                if let Some(value) = self.read_to_crlf(source) {
+                    self.state = ParserState::ParseComplete {
+                        value: RespValue::Integer(digits_to_int(value)?),
+                    };
+                    return self.process(source);
+                }
+            }
+            ParserState::ReadBulkStringSize => {
+                if let Some(value) = self.read_to_crlf(source) {
+                    let length = digits_to_int(value)?;
+
+                    self.buffer.clear();
+
+                    self.state = match length {
+                        0 => ParserState::ParseComplete {
+                            value: RespValue::BulkString(Vec::new()),
+                        },
+                        positive if positive > 0 => ParserState::ReadBulkStringData {
+                            length: length as usize,
+                        },
+                        negative if negative < 0 => ParserState::ParseComplete {
+                            value: RespValue::Null,
+                        },
+                        _ => panic!("this should be unreachable"),
+                    };
+
+                    return self.process(source);
+                }
+            }
+            ParserState::ReadBulkStringData { length } => {
+                let remaining = length - self.buffer.len() + 2;
+
+                if source.len() < remaining {
+                    self.buffer.extend_from_slice(&source.split()[..]);
+                } else {
+                    self.buffer
+                        .extend_from_slice(&source.split_to(remaining - 2)[..]);
+                    source.advance(2);
+                }
+
+                let string_buffer = std::mem::replace(&mut self.buffer, Vec::new());
+
+                eprintln!("Buffer: {}", utf8(&string_buffer)?);
+
+                self.state = ParserState::ParseComplete {
+                    value: RespValue::BulkString(string_buffer),
+                };
+
+                return self.process(source);
+            }
+            ParserState::ReadArraySize => {
+                if let Some(value) = self.read_to_crlf(source) {
+                    let length = digits_to_int(value)?;
+
+                    self.buffer.clear();
+
+                    self.state = match length {
+                        0 => ParserState::ParseComplete {
+                            value: RespValue::Array(Vec::new()),
+                        },
+                        positive if positive > 0 => ParserState::ReadArrayPart {
+                            length: length as usize,
+                            index: 0,
+                            values: Vec::with_capacity(length as usize),
+                            subparser: Box::new(Parser::new()),
+                        },
+                        negative if negative < 0 => ParserState::ParseComplete {
+                            value: RespValue::Null,
+                        },
+                        _ => panic!("this should be unreachable"),
+                    };
+
+                    return self.process(source);
+                }
+            }
+            ParserState::ReadArrayPart { .. } => {
+                // This feels really wrong...
+                let mut prev_state =
+                    std::mem::replace(&mut self.state, ParserState::WaitingForType);
+
+                if let ParserState::ReadArrayPart {
+                    length,
+                    mut index,
+                    mut values,
+                    mut subparser,
+                } = prev_state
+                {
+                    if let Some(parsed) = subparser.process(source)? {
+                        index += 1;
+                        values.push(parsed);
+                        subparser = Box::new(Parser::new());
+
+                        if index == length {
+                            self.state = ParserState::ParseComplete {
+                                value: RespValue::Array(values),
+                            };
+                        } else {
+                            self.state = ParserState::ReadArrayPart {
+                                length,
+                                index,
+                                values,
+                                subparser,
+                            };
+                        }
+
+                        return self.process(source);
+                    }
+
+                    self.state = ParserState::ReadArrayPart {
+                        length,
+                        index,
+                        values,
+                        subparser,
+                    };
+                }
+            }
+            ParserState::ParseComplete { .. } => {
+                self.buffer.clear();
+
+                // self is a mutable reference.  need to swap out state to move a value out of
+                // it without cloning it.
+                let prev_state = std::mem::replace(&mut self.state, ParserState::WaitingForType);
+                return Ok(Some(prev_state.into_value()));
+            }
+            _ => {
+                return Err(EngineError::unknown(&format!(
+                    "unimplemented state: {:?}",
+                    self.state
+                )))
+            }
+        };
+
+        Ok(None)
+    }
+}
+
 /// Helper to simplify stringifying bytes
 fn utf8(bytes: &[u8]) -> Result<String, EngineError> {
     Ok(std::str::from_utf8(bytes)
@@ -18,9 +287,8 @@ fn take_crlf<'a>(
         return Ok(None);
     }
 
-    let newline_offset = buffer[..]
+    let newline_offset = buffer[position..]
         .iter()
-        .skip(position)
         .zip(buffer[position + 1..].iter())
         .position(|(cr, lf)| *cr == b'\r' && *lf == b'\n');
 
@@ -36,7 +304,7 @@ fn take_crlf<'a>(
 
 /// Try to read a RESP simple string from buffer
 ///
-/// Position is expected to be __after__ the '*'
+/// Position is expected to be __after__ the '+'
 fn resp_read_simple_string(
     buffer: &mut BytesMut,
     position: usize,
@@ -54,7 +322,7 @@ fn resp_read_simple_string(
 
 /// Try to read a RESP integer from buffer
 ///
-/// Position is expected to be __after__ the '*'
+/// Position is expected to be __after__ the ':'
 fn resp_read_integer(
     buffer: &mut BytesMut,
     position: usize,
@@ -75,7 +343,7 @@ fn resp_read_integer(
 
 /// Try to read a RESP error from buffer
 ///
-/// Position is expected to be __after__ the '*'
+/// Position is expected to be __after__ the '-'
 fn resp_read_error(
     buffer: &mut BytesMut,
     position: usize,
@@ -197,7 +465,7 @@ fn resp_read_value(
 /// Encode value as a string
 fn resp_encode(value: &RespValue) -> Result<Vec<u8>, EngineError> {
     Ok(match value {
-        RespValue::SimpleString(s) | RespValue::Error(s) => {
+        RespValue::SimpleString(s) => {
             if s.contains('\n') {
                 return Err(EngineError::unknown(&format!(
                     "Value is not binary safe: {}",
@@ -206,6 +474,16 @@ fn resp_encode(value: &RespValue) -> Result<Vec<u8>, EngineError> {
             }
 
             format!("+{}\r\n", s).as_bytes().iter().copied().collect()
+        }
+        RespValue::Error(s) => {
+            if s.contains('\n') {
+                return Err(EngineError::unknown(&format!(
+                    "Value is not binary safe: {}",
+                    s
+                )));
+            }
+
+            format!("-{}\r\n", s).as_bytes().iter().copied().collect()
         }
         RespValue::Integer(i) => format!(":{}\r\n", i).as_bytes().iter().copied().collect(),
         RespValue::BulkString(bytes) => {
@@ -467,6 +745,149 @@ mod test {
             let value = from_buf(&mut buffer)?.expect("should have been found");
 
             assert_eq!(value, *expected);
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_resp_encode() -> TestResult {
+        let tests = [
+            (
+                "+Hello, World!\r\n",
+                RespValue::SimpleString(String::from("Hello, World!")),
+            ),
+            (
+                "-Error Message\r\n",
+                RespValue::Error(String::from("Error Message")),
+            ),
+            // NOTE: Null is always encoded as an array instead of a bulkstring.
+            // This may end up causing issues.
+            ("*-1\r\n", RespValue::Null),
+            ("$1\r\n?\r\n", RespValue::BulkString(vec![b'?'])),
+            (
+                "$4\r\nBulk\r\n",
+                RespValue::BulkString(vec![b'B', b'u', b'l', b'k']),
+            ),
+            (":0\r\n", RespValue::Integer(0)),
+            (":12\r\n", RespValue::Integer(12)),
+            (":-1234\r\n", RespValue::Integer(-1234)),
+            (":9999999\r\n", RespValue::Integer(9999999)),
+            ("*-1\r\n", RespValue::Null),
+            ("*0\r\n", RespValue::Array(vec![])),
+            (
+                "*1\r\n+Item 1\r\n",
+                RespValue::Array(vec![RespValue::SimpleString(String::from("Item 1"))]),
+            ),
+            (
+                "*2\r\n+Item 1\r\n:456\r\n",
+                RespValue::Array(vec![
+                    RespValue::SimpleString(String::from("Item 1")),
+                    RespValue::Integer(456),
+                ]),
+            ),
+            (
+                "*2\r\n+Item 1\r\n:456\r\n",
+                RespValue::Array(vec![
+                    RespValue::SimpleString(String::from("Item 1")),
+                    RespValue::Integer(456),
+                ]),
+            ),
+            (
+                "*3\r\n$6\r\nst\ning\r\n*2\r\n+SubItem1\r\n+SubItem2\r\n-Error\r\n",
+                RespValue::Array(vec![
+                    RespValue::BulkString(b"st\ning".to_vec()),
+                    RespValue::Array(vec![
+                        RespValue::SimpleString(String::from("SubItem1")),
+                        RespValue::SimpleString(String::from("SubItem2")),
+                    ]),
+                    RespValue::Error(String::from("Error")),
+                ]),
+            ),
+        ];
+
+        for (expected, value) in &tests {
+            let encoded = resp_encode(value)?;
+            assert_eq!(
+                &encoded[..],
+                expected.as_bytes(),
+                "{:?} != {}",
+                utf8(&encoded),
+                expected
+            );
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_state_machine_parser() -> TestResult {
+        let tests = [
+            (
+                "+Hello, World!\r\n",
+                RespValue::SimpleString(String::from("Hello, World!")),
+            ),
+            (
+                "-Error Message\r\n",
+                RespValue::Error(String::from("Error Message")),
+            ),
+            // NOTE: Null is always encoded as an array instead of a bulkstring.
+            // This may end up causing issues.
+            ("*-1\r\n", RespValue::Null),
+            ("$1\r\n?\r\n", RespValue::BulkString(vec![b'?'])),
+            (
+                "$4\r\nBulk\r\n",
+                RespValue::BulkString(vec![b'B', b'u', b'l', b'k']),
+            ),
+            (":0\r\n", RespValue::Integer(0)),
+            (":12\r\n", RespValue::Integer(12)),
+            (":-1234\r\n", RespValue::Integer(-1234)),
+            (":9999999\r\n", RespValue::Integer(9999999)),
+            ("*-1\r\n", RespValue::Null),
+            ("*0\r\n", RespValue::Array(vec![])),
+            (
+                "*1\r\n+Item 1\r\n",
+                RespValue::Array(vec![RespValue::SimpleString(String::from("Item 1"))]),
+            ),
+            (
+                "*2\r\n+Item 1\r\n:456\r\n",
+                RespValue::Array(vec![
+                    RespValue::SimpleString(String::from("Item 1")),
+                    RespValue::Integer(456),
+                ]),
+            ),
+            (
+                "*2\r\n+Item 1\r\n:456\r\n",
+                RespValue::Array(vec![
+                    RespValue::SimpleString(String::from("Item 1")),
+                    RespValue::Integer(456),
+                ]),
+            ),
+            (
+                "*3\r\n$6\r\nst\ning\r\n*2\r\n+SubItem1\r\n+SubItem2\r\n-Error\r\n",
+                RespValue::Array(vec![
+                    RespValue::BulkString(b"st\ning".to_vec()),
+                    RespValue::Array(vec![
+                        RespValue::SimpleString(String::from("SubItem1")),
+                        RespValue::SimpleString(String::from("SubItem2")),
+                    ]),
+                    RespValue::Error(String::from("Error")),
+                ]),
+            ),
+        ];
+
+        for (source, expected) in &tests {
+            eprintln!("Testing '{}'", source);
+            let mut buffer = BytesMut::from(source.as_bytes());
+            let mut parser = Parser::new();
+
+            let parsed = parser.process(&mut buffer)?;
+
+            assert!(parsed.is_some());
+            assert_eq!(parsed.unwrap(), *expected);
+
+            let should_not_parse = parser.process(&mut buffer)?;
+            assert!(should_not_parse.is_none());
         }
 
         Ok(())
